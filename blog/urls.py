60e6c5546882cf3407a86c7116e8412b7c096c66
landa44 <pablo44landa@gmail.com>
2021-12-19 19:33:39 -0500

    added base.html and fixed duplication tampletes

--- blog/urls.py
+++ blog/urls.py
@@ -3,12 +3,13 @@ from django.urls import path
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+app_name="public"
 urlpatterns = [
-    path("", views.index),
-    path("home/", views.index),
-    path("index/", views.index),
-    path("about/", views.about),
-    path("contact/", views.contact),
+    path("", views.index, name="index"),
+    path("index/", views.index, name="index"),
+    path("home/", views.index, name="index"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
     #   path('blog/', views.get_post),
 ]
 urlpatterns += staticfiles_urlpatterns()
